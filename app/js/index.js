40eb536ad103036f6e484ac97cb6f1e1d638f3b5
Sasha <oleksandr.zoria@masterofcode.com>
2020-05-05 10:27:04 +0300

    Finished product section mobile

--- app/js/index.js
+++ app/js/index.js
@@ -6,3 +6,12 @@ nodesArray.forEach((item) => {
 	const width = item.getAttribute("data-width");
 	item.style.width = `${width}%`;
 })
+
+const toggler = document.getElementById('toggle');
+const target = document.getElementById('target');
+const targetIcon = document.getElementById('target-icon');
+
+toggler.addEventListener('click', function () {
+	target.classList.toggle('active');
+	targetIcon.classList.toggle('open');
+})
